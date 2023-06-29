93a216027ecc989b0244633aacd4615763fd4d8a
phazel <fazel8195@gmail.com>
2023-06-29 16:34:53 +0330

    Removed custom imports, Updated project.

--- joi2yup.js
+++ joi2yup.js
@@ -14,9 +14,9 @@ module.exports = (data, var_name, type_name, minify) => {
             .replaceAll('\t', '')
             .replaceAll('  ', '')
             .replaceAll('\r\n', '')
-        result = `import * as Yup from 'yup';import '../yup/convert_date';import '../yup/convert_num';export const ${var_name} = Yup.object(${str});export type ${type_name} = Yup.InferType<typeof ${var_name}>;`
+        result = `import * as Yup from 'yup';export const ${var_name} = Yup.object(${str});export type ${type_name} = Yup.InferType<typeof ${var_name}>;`
     } else {
-        result = `import * as Yup from 'yup';\nimport '../yup/convert_date'\nimport '../yup/convert_num'\nexport const ${var_name} = Yup.object(${str});\nexport type ${type_name} = Yup.InferType<typeof ${var_name}>;`
+        result = `import * as Yup from 'yup';\nexport const ${var_name} = Yup.object(${str});\nexport type ${type_name} = Yup.InferType<typeof ${var_name}>;`
     }
     return result
 }
